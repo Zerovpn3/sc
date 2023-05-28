6b5b41f5a2bc0d81eb0b6b00997e86d97623bdb5
Zerovpn3 <134721008+Zerovpn3@users.noreply.github.com>
2023-05-28 19:57:32 +0700

    Update ipsec.sh

--- ipsec/ipsec.sh
+++ ipsec/ipsec.sh
@@ -35,7 +35,7 @@ echo "Checking VPS"
 clear
 # ==================================================
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/anuy639/sc/main/ipsec"
+akbarvpn="raw.githubusercontent.com/zerovpn3/sc/main/ipsec"
 
 VPN_IPSEC_PSK='myvpn'
 NET_IFACE=$(ip -o $NET_IFACE -4 route show to default | awk '{print $5}');
